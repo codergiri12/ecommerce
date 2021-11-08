38269b1ff5011ccdc388f257110029bcbef45b2a
Giri sai <66117620+codergiri12@users.noreply.github.com>
2021-11-08 20:04:45 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -89,7 +89,7 @@ function App({history}) {
 
         <Route exact path="/contact" component={Contact} />
 
-        <Route exact path="/about" component={About} />
+        <Route exact path="/about" component={Home} />
 
         <ProtectedRoute exact path="/account" component={Profile} />
 
